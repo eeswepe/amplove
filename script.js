5b5eaa4a56f7e0028615aebe06b4f4a5208dbba7
singgih wahyu permana <singgihwahyupermana@gmail.com>
2023-02-03 19:17:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const heart = document.getElementById("heart")
+
+heart.addEventListener("click", () => {
+    alert("halo cimolll")
+    alert("ini cinggaa")
+    alert("atapuu")
+    alert("maaf cuma sederhana gini")
+    alert("teks biasa aja")
+    alert("lanjut yaa")
+    window.location.href = "./img.html"
+})
